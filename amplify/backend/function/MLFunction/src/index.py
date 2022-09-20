642b7b2647306faa931e6cff4e7cb2fd8a21f8c2
Warren Kavanagh <warrenkavanagh1000@gmail.com>
2022-09-20 18:48:07 +0100

    Tried to add requests dependency does not invoke locally

--- amplify/backend/function/MLFunction/src/index.py
+++ amplify/backend/function/MLFunction/src/index.py
@@ -1,15 +1,18 @@
 import json
+import requests
 
 def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from lambda!')
-  }
+    print('received event:')
+    print(event)
+    feed = "https://abcnews.go.com/abcnews/moneyheadlines"
+    response = requests.get(feed)
+    print(response)
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': json.dumps('Hello from lambda!!')
+    }
